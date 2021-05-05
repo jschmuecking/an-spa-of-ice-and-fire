20e9bca977c5c20be0bff25d3303d169128eb657
jschmuecking <jochen.schmuecking@prodyna.com>
2021-05-05 23:37:24 +0200

    prettier eslint config + rule adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,41 @@
 module.exports = {
   extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended'
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: ['react', '@typescript-eslint', 'jest'],
+  plugins: ["react", "@typescript-eslint", "jest"],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
   rules: {
-    'linebreak-style': 'off',
-    'prettier/prettier': [
-      'error',
+    curly: ["error", "all"],
+    "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
+        endOfLine: "auto",
       },
     ],
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
   },
 };
